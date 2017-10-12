4419d6e0517fff820983bd17de01e5b4fa4623ee
Edimarcordero0220 <edicordero98@gmail.com>
2017-10-13 00:51:24 +0200

    Bregando

---
+++ Tarea_6/Entidades/Categorias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Categorias
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Tarea_6/Tarea_6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tarea_6.Global" Language="C#" %>
